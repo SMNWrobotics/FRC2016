00a5760f5f6dfbc902ecc19ec8c75dd100bf5123
bkeeney <bobk@bkeeney.com>
2016-03-06 15:16:28 -0600

    Build 102.  Mac/Windows installers.

---
+++ FRC Scout/Scripts/BuildDMG.sh
@@ -0,0 +1,2 @@
+/usr/local/bin/dmgcanvas /Volumes/Raptor/Bob\'s\ Projects/Subversion/FRC2016/FRC\ Scout/Scripts/FRCScout.dmgCanvas /Volumes/Raptor/Bob\'s\ Projects/Subversion/FRC2016/FRC\ Scout/FRCScoutingApp.dmg
+	
